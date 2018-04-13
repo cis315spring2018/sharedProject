cda9b15238b2362f2bdddee52655c9c26d699acc
Arthur Manning <atmanning@emmaus.edu>
2018-04-13 08:17:33 -0500

    Initial project shared with my name comment

---
+++ sharedProject/src/sharedProject/JFrameMain.java
@@ -0,0 +1,54 @@
+/**
+ *   file: JFrameMain.java
+ */
+package sharedProject;
+
+import java.awt.BorderLayout;
+import java.awt.EventQueue;
+
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.border.EmptyBorder;
+import javax.swing.JButton;
+
+/**
+ * @author atmanning - atmanning@dbq.edu
+ * manning's main JFrame - with some buttons
+ *
+ */
+public class JFrameMain extends JFrame {
+
+	private JPanel contentPane;
+
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					JFrameMain frame = new JFrameMain();
+					frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+
+	/**
+	 * Create the frame.
+	 */
+	public JFrameMain() {
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setBounds(100, 100, 450, 300);
+		contentPane = new JPanel();
+		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
+		setContentPane(contentPane);
+		contentPane.setLayout(null);
+		
+		JButton btnNewButton = new JButton("New button");
+		btnNewButton.setBounds(93, 64, 97, 25);
+		contentPane.add(btnNewButton);
+	}
+}
